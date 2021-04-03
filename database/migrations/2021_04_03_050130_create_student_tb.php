07928386395c982f64d9d40cdd119553503b187e
sayfatay <sayfatay1234@gmail.com>
2021-04-03 12:13:04 +0700

    first commit

---
+++ database/migrations/2021_04_03_050130_create_student_tb.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentTb extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('seudent', function (Blueprint $table) {
+            $table->id();
+            $table->integer('main_id');
+            $table->string('fname');
+            $table->string('lname');
+            $table->string('subject');
+            $table->string('score');
+            $table->string('grade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('seudent');
+    }
+}
